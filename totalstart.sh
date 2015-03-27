a9fc2f29f4ed87c3ee0280745c1348df4256287e
Your Name <1004xlsrn@gmail.com>
2015-03-27 11:32:35 +0000

    complete script

---
+++ totalstart.sh
@@ -0,0 +1,16 @@
+#install butterfly
+cd butterfly
+./start.sh
+#install bind9
+cd ../bind9
+./start.sh
+#install nginx
+cd ../nginx
+./start.sh
+#install guacamole
+cd ../guacamole
+./start.sh
+#install ssh
+cd ../ssh
+./start.sh
+
