3602e4622e97a2ebf7cb780e8be78af151d8c947
Your Name <1004xlsrn@gmail.com>
2015-03-27 11:24:20 +0000

    complete nginx

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,6 +1,28 @@
+#!/bin/bash
+
+PackageInstalledCheck(){
+	packagename=$1
+	installstr="install ok installed"
+	isinstalled=$(dpkg -s $packagename | grep "$installstr")
+	if [ "$isinstalled" ] 
+	then
+		echo "$packagename is already installed"
+	else
+		aptitude install -y $packagename
+	fi
+}
 #install nginx
+PackageInstalledCheck nginx
 #move available-site
+sitedir="sites-available"
+cp -r "$sitedir" /etc/nginx/
 #enable available-site except copy-default
-#enable modules
+declare -a filelist
+filelist=(`ls /etc/nginx/"$sitedir" | grep -v "copy-default"`)
+for flist in ${filelist[*]}
+do
+	./ngensite "$flist"
+done
 #restart the server
+nginx -s reload
 
