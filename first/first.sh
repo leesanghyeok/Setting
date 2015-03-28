a17033fa2308a588b32bbded491de3a035ac20b9
Your Name <1004xlsrn@gmail.com>
2015-03-28 04:37:55 +0000

    bug fix first.sh

--- first/first.sh
+++ first/first.sh
@@ -1,11 +1,18 @@
 #apt update
 aptitude update
-aptitude upgrade
+aptitude -y upgrade
 #set username
-username="forhack"
-useradd -s /bin/bash -m -d /home/"$username" -g root "$username"
-passwd "$username"
-cp sshd_config /etc/ssh/
-echo "you must save in /etc/sudoers"
-visudoo
-service	ssh restart
+username="$1"
+if [ "$username" ]
+then
+	useradd -s /bin/bash -m -d /home/"$username" -g root "$username"
+	passwd "$username"
+	cp sshd_config /etc/ssh/
+	echo "you must save in /etc/sudoers"
+	visudo
+	service	ssh restart
+else
+	echo "please put the username"
+fi
+
+
