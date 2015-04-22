de79bacf2effc63df1117f7c1d5adab2947551c8
Your Name <1004xlsrn@gmail.com>
2015-04-22 14:49:37 +0000

    guacamole upgrade 0.9.6.ver start script

--- guacamole/start.sh
+++ guacamole/start.sh
@@ -22,9 +22,10 @@ IsFileCheck(){
 	fi
 }
 #declare
-defaultname="guacamole-0.9.5"
+ver="0.9.6"
+defaultname="guacamole-$ver"
 clientname="$defaultname.war"
-servername="guacamole-server-0.9.5"
+servername="guacamole-server-$ver"
 tarservername="$servername.tar.gz"
 #install
 declare -a installlist
@@ -50,29 +51,29 @@ cd ..
 mkdir /var/lib/guacamole
 mkdir /etc/guacamole
 mkdir /usr/share/tomcat7/.guacamole
-cp guacamole-0.9.5.war /var/lib/guacamole/guacamole.war
+cp guacamole-"$ver".war /var/lib/guacamole/guacamole.war
 cp ../guacamole.properties /etc/guacamole/guacamole.properties
 ln -s /var/lib/guacamole/guacamole.war /var/lib/tomcat7/webapps
 ln -s /etc/guacamole/guacamole.properties /usr/share/tomcat7/.guacamole/
-service tomcat7 restart
 service guacd restart
 cd ..
 rm -r guac_mole
 #////////mysql authentication//////////
-wget http://sourceforge.net/projects/guacamole/files/current/extensions/guacamole-auth-mysql-0.9.5.tar.gz
+wget http://sourceforge.net/projects/guacamole/files/current/extensions/guacamole-auth-jdbc-"$ver".tar.gz
 wget http://cdn.mysql.com/Downloads/Connector-J/mysql-connector-java-5.1.35.tar.gz
-tar -xzf guacamole-auth-mysql-0.9.5.tar.gz
+tar -xzf guacamole-auth-jdbc-"$ver".tar.gz
 tar -xzf mysql-connector-java-5.1.35.tar.gz
-rm guacamole-auth-mysql-0.9.5.tar.gz
+rm guacamole-auth-jdbc-"$ver".tar.gz
 rm mysql-connector-java-5.1.35.tar.gz
 mkdir /var/lib/guacamole/classpath
-cp guacamole-auth-mysql-0.9.5/lib/* /var/lib/guacamole/classpath/
+cp guacamole-auth-jdbc-"$ver"/mysql/guac* /var/lib/guacamole/classpath/
 cp mysql-connector-java-5.1.35/mysql-connector-java-5.1.35-bin.jar /var/lib/guacamole/classpath
 #//////////setting the mysql/////////
 echo "CREATE DATABASE guacamole;CREATE USER 'guac'@'%' IDENTIFIED BY 'wptjfdk';GRANT SELECT,INSERT,UPDATE,DELETE ON guacamole.* TO 'guac'@'%';FLUSH PRIVILEGES;" | mysql -u root --password=wptjfdk
-cat guacamole-auth-mysql-0.9.5/schema/*.sql | mysql -u root -p guacamole
-rm -r guacamole-auth-mysql-0.9.5/
+cat guacamole-auth-jdbc-"$ver"/mysql/schema/*.sql | mysql -u root --password=wptjfdk guacamole
+rm -r guacamole-auth-jdbc-"$ver"/
 rm -r mysql-connector-java-5.1.35/
 #//////////setting the tomcat7///////
 cp server.xml /etc/tomcat7/server.xml
+service tomcat7 restart
 echo "complete the guacamole setting!"
